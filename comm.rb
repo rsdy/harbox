904037cbfe775cd54ec185634fb8de900d1f1fd9
rhapsodhy <me@rhapsodhy.hu>
2012-07-03 20:52:08 +0200

    improved testing of communication with the device

--- comm.rb
+++ comm.rb
@@ -4,17 +4,35 @@ require 'rubygems'
 require 'serialport'
 require 'openssl'
 
-ser = SerialPort.new ARGV[0], ARGV[1].to_i, 8, 1, SerialPort::NONE
+@ser = SerialPort.new ARGV[0], ARGV[1].to_i, 8, 1, SerialPort::NONE
 
-msg = ARGV[2]
-ser.putc 'e'
-ser.putc msg.length + 20
-ser.puts "#{msg}#{OpenSSL::HMAC.digest('sha1', 'asdf', msg)}"
-ser.flush
-
-sleep 1
-if buf = ser.read
-  print buf
-  $stdout.flush
+def put_data cmd, msg, key
+  @ser.putc cmd
+  @ser.putc msg.length + 20
+  @ser.puts "#{msg}#{OpenSSL::HMAC.digest('sha1', key, msg)}"
+  @ser.flush
 end
-ser.close
+
+def run_test name, cmd, msg, expected, key
+  put_data cmd, msg, key
+  sleep 0.1
+  answer = @ser.read.strip
+  if answer != expected
+    puts "FAIL - #{name} - received: #{answer}"
+    return
+  end
+
+  puts "OK - #{name}"
+end
+
+1.upto(5) { |x|
+  [['echo',       'e', 'test_msg_1', 'test_msg_1', 'asdf'],
+   ['wrong-echo', 'e', 'whatevz', '', 'asdf213'],
+   ['echo',       'e', 'test_msg_2', 'test_msg_2', 'asdf'],
+   ['wrong-command', ';', 'whatevz', '', 'asdf213']
+  ].each { |args| run_test *args }
+
+  puts '---------------------'
+}
+
+@ser.close
